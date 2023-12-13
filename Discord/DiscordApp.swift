0722636441d8e28e5e31bb5aa2748a7a3fc56857
oceaniswater <markflyingbear@gmail.com>
2023-12-13 13:56:35 +0000

    Initial Commit

---
+++ Discord/DiscordApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscordApp.swift
+//  Discord
+//
+//  Created by Mark Golubev on 13/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct DiscordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
