206fa127cb3c7ba3ac56c6b9a891e320134b15ca
oceaniswater <markflyingbear@gmail.com>
2023-12-15 15:46:01 +0000

    create menu view with a smooth channels animation

--- Discord/Home/MenuView.swift
+++ Discord/Home/MenuView.swift
@@ -13,13 +13,37 @@ struct Server: Identifiable {
     let name: String
 }
 
+struct Chanel: Identifiable {
+    var id: Int?
+    let createdAt: Date
+    let name: String
+    let type: ChanelType
+}
+
+enum ChanelType {
+    case text
+    case voice
+}
+
 struct MenuView: View {
+    @State var selectedChanel: Chanel?
+    @State var showTextChanels = true
+    @State var showVoiceChanels = true
+
+    
     var mockServers: [Server] = [
         Server(id: 1, createdAt: .now, name: "Swift"),
         Server(id: 2, createdAt: .now, name: "Python"),
         Server(id: 3, createdAt: .now, name: "Kotlin"),
         Server(id: 4, createdAt: .now, name: "C++"),
         Server(id: 5, createdAt: .now, name: "Go")
+    ]
+    
+    var mockChanel: [Chanel] = [
+        Chanel(id: 1, createdAt: .now, name: "general", type: .text),
+        Chanel(id: 2, createdAt: .now, name: "ux/ui", type: .text),
+        Chanel(id: 3, createdAt: .now, name: "vision pro", type: .text),
+        Chanel(id: 4, createdAt: .now, name: "wwdc talks", type: .voice)
     ]
     
     var body: some View {
@@ -66,14 +90,137 @@ struct MenuView: View {
                 VStack {
                     HStack {
                         Text("Swift")
+                            .bold()
                         Spacer()
                         Image(systemName: "ellipsis")
+                            .fontWeight(.heavy)
                         
                     }
                     .padding()
                     
                     ScrollView {
+                        HStack(alignment: .center, spacing: 5) {
+                            Button(action: {
+                                
+                            }, label: {
+                                ZStack {
+                                    RoundedRectangle(cornerSize: CGSize(width: 100, height: 20))
+                                        .fill(Color.black)
+                                        .frame(height: 40)
+                                    
+                                    HStack {
+                                        Image(systemName: "magnifyingglass")
+                                            .font(.callout)
+                                            .foregroundStyle(Color.white)
+                                        Text("Search")
+                                            .font(.callout)
+                                            .foregroundStyle(Color.white)
+                                        Spacer()
+                                    }
+                                    .padding(.horizontal, 10)
+                                }
+                            })
+                            
+                            Button(action: {
+                                
+                            }, label: {
+                                Image(systemName: "person.fill.badge.plus")
+                                    .imageScale(.large)
+                                    .foregroundStyle(.white)
+                                    .padding(8)
+                                    .background {
+                                        Circle()
+                                            .fill(Color.black)
+                                    }
+                            })
+                        }
+                        .padding(.horizontal)
                         
+                        Button {
+                            withAnimation {
+                                showTextChanels.toggle()
+                            }
+                        } label: {
+                            HStack(spacing: 0) {
+                                Image(systemName: showTextChanels ? "chevron.down" : "chevron.right")
+                                    .font(.footnote)
+                                    .frame(width: 24)
+                                Text("Text Chanels")
+                                    .font(.callout)
+                                Spacer()
+                            }
+                            .foregroundStyle(Color.gray)
+                            .padding(.horizontal, 3)
+                            .padding(.vertical, 5)
+                        }
+
+                        
+                        if showTextChanels {
+                            ForEach(mockChanel) { chanel in
+                                if chanel.type == .text {
+                                    Button {
+                                        withAnimation {
+                                            selectedChanel = chanel
+                                        }
+                                    } label: {
+                                        HStack {
+                                            Image(systemName: "number")
+                                                .foregroundStyle(Color.gray)
+                                            Text(chanel.name)
+                                                .foregroundStyle(selectedChanel?.id == chanel.id ? Color.white : Color.gray)
+                                                .font(.title3)
+                                        }
+                                    }
+                                    .padding(.vertical, 5)
+                                    .padding(.horizontal)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+
+                                }
+                            }
+                        }
+                        
+                        Button {
+                            withAnimation {
+                                showVoiceChanels.toggle()
+                            }
+                        } label: {
+                            HStack(spacing: 0) {
+                                Image(systemName: showVoiceChanels ? "chevron.down" : "chevron.right")
+                                    .font(.footnote)
+                                    .frame(width: 24)
+                                Text("Voice Chanels")
+                                    .font(.callout)
+                                Spacer()
+                            }
+                            .foregroundStyle(Color.gray)
+                            .padding(.horizontal, 3)
+                            .padding(.vertical, 5)
+                        }
+
+                        
+                        if showVoiceChanels {
+                            ForEach(mockChanel) { chanel in
+                                if chanel.type == .voice {
+                                    Button {
+                                        withAnimation {
+                                            selectedChanel = chanel
+                                        }
+                                    } label: {
+                                        HStack {
+                                            Image(systemName: "number")
+                                                .foregroundStyle(Color.gray)
+                                            Text(chanel.name)
+                                                .foregroundStyle(selectedChanel?.id == chanel.id ? Color.white : Color.gray)
+                                                .font(.title3)
+                                        }
+                                    }
+                                    .padding(.vertical, 5)
+                                    .padding(.horizontal)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+
+                                }
+                            }
+                        }
                     }
                 }
                 .frame(width: geometry.size.width - 60)
@@ -85,9 +232,6 @@ struct MenuView: View {
             }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
-//        .background {
-//            Color(uiColor: .black)
-//        }
         .preferredColorScheme(.dark)
     }
 }
